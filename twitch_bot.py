9a4bb29cf3fdc6f701b31cc02420da0ff27bf98b
DyadushkaFox <139276481+DyadushkaFox@users.noreply.github.com>
2025-03-01 02:21:13 +0300

    Update twitch_bot.py

--- twitch_bot.py
+++ twitch_bot.py
@@ -1,9 +1,9 @@
+import time
+import requests
 import os
 import random
-import requests
-import time
 from dotenv import load_dotenv
-from twitchio.ext import commands
+from twitchio import Client, Message
 
 # Загружаем переменные окружения
 load_dotenv('fox.env')
@@ -19,39 +19,60 @@ questions = [
     "Какой твой любимый жанр игр?",
     "Что думаешь о последних обновлениях в игре?",
     "Какую игру ты планируешь пройти в ближайшее время?",
+    "Какие твои любимые игры в жанре RPG?",
+    "Кто твой любимый персонаж в играх?",
+    "Какую игру ты считаешь самой сложной?",
+    "Какие стримы ты обычно смотришь на Twitch?",
+    "Какие игры ты больше всего ждешь в следующем году?",
+    "Что ты думаешь о VR-играх?",
+    "Какую игру ты бы порекомендовал друзьям?",
+    # Добавьте остальные вопросы
 ]
 
+# Функция для проверки, идет ли стрим
+def check_stream_status():
+    url = f'https://api.twitch.tv/helix/streams?user_login={TWITCH_CHANNEL}'
+    headers = {
+        'Client-ID': TWITCH_CLIENT_ID,
+        'Authorization': f'Bearer {TWITCH_OAUTH_TOKEN}'
+    }
+    response = requests.get(url, headers=headers)
+    data = response.json()
+
+    if data['data']:
+        return True  # Стрим идет
+    return False  # Стрим не идет
+
 # Основной класс бота
-class TwitchBot(commands.Bot):
+class TwitchBot(Client):
 
     def __init__(self):
-        super().__init__(
-            token=TWITCH_OAUTH_TOKEN,
-            prefix="!",
-            initial_channels=[TWITCH_CHANNEL]
-        )
+        super().__init__(token=TWITCH_OAUTH_TOKEN)
 
     async def event_ready(self):
         print(f'Bot {TWITCH_BOT_USERNAME} is online and ready to chat!')
+        
+        # Пока стрим не начнется, ждем его
+        while not check_stream_status():  
+            print("Stream is offline. Waiting for stream to go live...")
+            time.sleep(60)  # Проверяем каждую минуту
+
+        print("Stream is live! Starting bot...")
         await self.send_random_question()
 
     async def send_random_question(self):
-        while True:
-            question = random.choice(questions)
-            await self.connected_channels[0].send(question)
-            await self.wait_for_ready()
-            await asyncio.sleep(1800)  # Отправляет вопрос каждые 30 минут
+        # Выбираем случайный вопрос
+        question = random.choice(questions)
+        await self.channel.send(question)
 
-    async def event_message(self, message):
+    async def event_message(self, message: Message):
+        # Ожидаем сообщений и реагируем
         print(f'{message.author.name}: {message.content}')
-        await self.handle_commands(message)
-
 
 # Функция для запуска бота
 def run_bot():
     bot = TwitchBot()
     bot.run()
-
 
 if __name__ == '__main__':
     run_bot()
